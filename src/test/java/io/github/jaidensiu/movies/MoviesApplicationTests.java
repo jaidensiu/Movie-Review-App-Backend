480cfa688c40c6912bce7e7ef57de8fdccb8f529
Jaiden Siu <jaidensiu@gmail.com>
2023-12-01 02:26:55 -0800

    initial commit

---
+++ src/test/java/io/github/jaidensiu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaidensiu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
